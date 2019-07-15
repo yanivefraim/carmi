104f0bdb32365aae5df9b28256c79d9697a3c063
Yaniv Efraim <yaniv.efraim@gmail.com>
2019-07-15 11:42:46 +0300

    Revert "transpile for args"
    This reverts commit 62df830122af500af43fbf22953938edbd99bb30.

--- src/templates/naive.js
+++ src/templates/naive.js
@@ -108,10 +108,7 @@ function base() {
       }
     }
 
-    function $setter(func) {
-      for (var _len = arguments.length, args = new Array(_len > 1 ? _len - 1 : 0), _key = 1; _key < _len; _key++) {
-        args[_key - 1] = arguments[_key];
-      }
+    function $setter(func, ...args) {
       if ($inBatch || $inRecalculate || $batchingStrategy) {
         $batchPending.push({ func, args });
         if ((!$inBatch && !$inRecalculate) && $batchingStrategy) {
